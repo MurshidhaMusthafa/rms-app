d17c6754cb98939e98a526554d4ddd86e767da53
murshidha <murshidhamusthafapt@gmail.com>
2022-08-24 10:11:38 +0530

    binded

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Login() {
+  const defaultEntity = {
+    userName: "",
+    passWord: "",
+  };
+
+  const [login, setLogin] = useState(defaultEntity);
+
+  // const [item, setItem] = useState(defaultEntity);
+  const onValueChange = (e) => {
+    console.log(e);
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
+  const onSubmit = () => {
+    console.log(login);
+  };
+
   return (
     <div className="relative">
       <div
@@ -19,18 +36,26 @@ function Login() {
             <div className="pt-7">
               <input
                 type="text"
+                name="userName"
+                value={login.userName}
+                onChange={onValueChange}
                 placeholder="User name"
-                className=" w-[418px] h-[55px] placeholder-white pl-5 text-xs font-medium rounded-lg bg-transparent border border-[#000000] "
+                className=" w-[418px] h-[55px] placeholder-white pl-5 text-xs font-medium rounded-lg bg-transparent border text-white border-[#000000] "
               ></input>
               <div className="pt-1">
                 <input
                   type="text"
+                  name="passWord"
+                  value={login.passWord}
+                  onChange={onValueChange}
                   placeholder="Password"
-                  className=" w-[418px] h-[55px] pl-5 text-xs placeholder-white bg-transparent font-medium rounded-lg border border-[#000000] "
+                  className=" w-[418px] h-[55px] pl-5 text-xs text-white placeholder-white bg-transparent font-medium rounded-lg border border-[#000000] "
                 ></input>
                 <div className="flex  justify-center items-center">
                   <button
-                    type="submit"
+                    type="button"
+                    onClick={onSubmit}
+                    to="/src/components/Login.js"
                     className="bg-gradient-to-r from-[#006ACB] to-[#031EAC] mt-4 text-white rounded-lg w-[252px] h-[55px] font-semibold text-xs text-align-center border-2 border-[#006ACB]"
                   >
                     LOGIN
